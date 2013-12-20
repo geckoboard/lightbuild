239ff553613056928655b479fc57ea9d70d3ab13
Leo Cassarani <leo.cassarani@me.com>
2013-12-20 14:30:27 +0000

    Fetch the build status from Jenkins's JSON API

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 var Router = require('./lib/router'),
+    Jenkins = require('./lib/jenkins'),
     env = process.env,
     refreshRate = 5;
 
-console.log("Autodiscovering router...");
+console.log("Autodiscovering Hue router...");
 
 Router.autodiscover(function(router) {
-  console.log("Found router: " + router.inspect());
+  console.log("Found Hue router: " + router.inspect());
 
   router.getLightbulb(env.LIGHTBULB_ID, function(lightbulb) {
     setInterval(function() {
-      tick(lightbulb);
+      jenkins = new Jenkins({
+        username: env.JENKINS_USERNAME,
+        password: env.JENKINS_PASSWORD,
+        hostname: env.JENKINS_HOSTNAME
+      });
+
+      tick(jenkins, lightbulb);
     }, refreshRate * 1000);
   });
 });
 
-function tick(lightbulb) {
-  lightbulb.blinkRed();
+function tick(jenkins, lightbulb) {
+  jenkins.getBuildStatus(function(status) {
+    if (status.isBroken()) {
+      lightbulb.blinkRed();
+    }
+    else {
+      lightbulb.showGreen();
+    }
+  });
 }
