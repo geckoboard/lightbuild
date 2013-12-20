2e7165d5898bb4d6a60234a335a55c2ab7b1d30d
Leo Cassarani <leo.cassarani@me.com>
2013-12-20 13:57:30 +0000

    Add a simple Lightbulb module and fetch the one with the given ID

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-var Router = require('./lib/router');
+var Router = require('./lib/router'),
+    env = process.env,
+    refreshRate = 5;
 
 console.log("Autodiscovering router...");
 
 Router.autodiscover(function(router) {
   console.log("Found router: " + router.inspect());
+
+  router.getLightbulb(env.LIGHTBULB_ID, function(lightbulb) {
+    setInterval(function() {
+      tick(lightbulb);
+    }, refreshRate * 1000);
+  });
 });
+
+function tick(lightbulb) {
+  lightbulb.blinkRed();
+}
