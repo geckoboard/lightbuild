5f8ade2df84eac04384e8a488f77804ff5caecd1
Leo Cassarani <leo.cassarani@me.com>
2013-12-20 15:19:53 +0000

    Extract a startTicking() function from the main event loop

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
 var Router = require('./lib/router'),
     Jenkins = require('./lib/jenkins'),
     env = process.env,
-    refreshRate = 5;
+    refreshRate = 30;
 
 console.log("Autodiscovering Hue router...");
 
 Router.autodiscover(function(router) {
-  var lightbulbId = env.LIGHTBULB_ID || 1;
+  var lightbulbId = env.LIGHTBULB_ID || 1,
+      buildServer = new Jenkins({
+        username: env.JENKINS_USERNAME,
+        password: env.JENKINS_PASSWORD,
+        hostname: env.JENKINS_HOSTNAME
+      });
 
   console.log("Found Hue router: " + router.inspect());
 
   router.getLightbulb(lightbulbId, function(lightbulb) {
-    var jenkins = new Jenkins({
-      username: env.JENKINS_USERNAME,
-      password: env.JENKINS_PASSWORD,
-      hostname: env.JENKINS_HOSTNAME
-    });
-
-    setInterval(function() {
-      tick(jenkins, lightbulb);
-    }, refreshRate * 1000);
-
-    tick(jenkins, lightbulb);
+    startTicking(buildServer, lightbulb);
   });
 });
 
-function tick(jenkins, lightbulb) {
-  jenkins.getBuildStatus(function(status) {
+function startTicking(buildServer, lightbulb) {
+  setInterval(function() {
+    tick(buildServer, lightbulb);
+  }, refreshRate * 1000);
+
+  tick(buildServer, lightbulb);
+}
+
+function tick(buildServer, lightbulb) {
+  buildServer.getBuildStatus(function(status) {
     if (status.isBroken()) {
       lightbulb.blinkRed();
     }
