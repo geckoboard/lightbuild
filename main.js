c5e34937762fe0da41d30c925f1c6906d429983f
Leo Cassarani <leo.cassarani@me.com>
2014-01-10 11:22:26 +0000

    It's not a router it's a bridge

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var Router = require('./lib/router'),
+var Bridge  = require('./lib/bridge'),
     Jenkins = require('./lib/jenkins'),
-    config = require('./config/jenkins').jenkins,
+    config  = require('./config/jenkins').jenkins,
     env = process.env,
     refreshRate = 30,
     buildServer = new Jenkins({
@@ -9,34 +9,34 @@ var Router = require('./lib/router'),
       hostname: config.hostname
     });
 
-console.log("Autodiscovering Hue router...");
+console.log("Autodiscovering Hue bridge...");
 
-Router.autodiscover(function(err, router) {
+Bridge.autodiscover(function(err, bridge) {
   if (err) {
     console.log("Error: " + err.message);
     process.exit(1);
   }
 
-  console.log("Found Hue router: " + router.inspect());
+  console.log("Found Hue bridge: " + bridge.inspect());
 
-  ensureAuthenticated(router, function() {
-    router.getLightbulb(env.LIGHTBULB_ID || 1, function(lightbulb) {
+  ensureAuthenticated(bridge, function() {
+    bridge.getLightbulb(env.LIGHTBULB_ID || 1, function(lightbulb) {
       startTicking(buildServer, lightbulb);
     });
   });
 });
 
-function ensureAuthenticated(router, callback) {
-  router.isAuthenticated(function(isAuthenticated) {
+function ensureAuthenticated(bridge, callback) {
+  bridge.isAuthenticated(function(isAuthenticated) {
     if (isAuthenticated) {
       callback();
     } else {
-      console.log("Requesting permissions to connect to your Hue router...");
+      console.log("Requesting permissions to connect to your Hue bridge...");
 
-      router.authenticate(function(success) {
+      bridge.authenticate(function(success) {
         if (!success) {
-          console.log("\nError: Lightbuild must be granted permission to connect to your Hue Router");
-          console.log("Please press the link button on top of the router, then run lightbuild again within 30 seconds\n");
+          console.log("\nError: Lightbuild must be granted permission to connect to your Hue bridge");
+          console.log("Please press the link button on top of the bridge, then run lightbuild again within 30 seconds\n");
           process.exit(1);
         }
 
